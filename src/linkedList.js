cf89d6b17e39c390d62051a8deade6cee4e7c18a
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-18 04:12:23 +0530

    refactor: prettify

--- src/linkedList.js
+++ src/linkedList.js
@@ -1,157 +1,150 @@
 import node from "./node.js";
 
 export default function linkedList() {
+  let head, tail, size;
+  head = null;
+  tail = null;
+  size = 0;
 
-    let head, tail, size;
-    head = null;
-    tail = null;
-    size = 0;
+  function append(value) {
+    const newNode = node();
 
-    function append(value) {
-        const newNode = node();
-        
-        newNode.value = value;
+    newNode.value = value;
 
-        if (!head) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            tail.nextNode = newNode;
-            tail = newNode;
-        }
-
-        size++;
+    if (!head) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail.nextNode = newNode;
+      tail = newNode;
     }
 
-    function prepend(value) {
-        const newNode = node();
-        
-        newNode.value = value;
+    size++;
+  }
 
-        if (!head) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            newNode.nextNode = head;
-            head = newNode;
-        }
+  function prepend(value) {
+    const newNode = node();
 
-        size++;
+    newNode.value = value;
+
+    if (!head) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      newNode.nextNode = head;
+      head = newNode;
     }
 
-    function getSize() {
-        return size;
+    size++;
+  }
+
+  function getSize() {
+    return size;
+  }
+
+  function getHead() {
+    return head;
+  }
+
+  function getTail() {
+    return tail;
+  }
+
+  function at(index) {
+    if (index < 0 || index >= size) {
+      return null;
     }
 
-    function getHead() {
-        return head;
+    let currentNode = head;
+
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.nextNode;
     }
 
-    function getTail() {
-        return tail;
+    return currentNode;
+  }
+
+  function pop() {
+    if (!head) {
+      return;
     }
 
-    function at(index) {
-        if (index < 0 || index >= size) {
-            return null;
-        }
+    if (size === 1) {
+      head = null;
+      tail = null;
+    } else {
+      let currentNode = head;
 
-        let currentNode = head;
-        
-        for (let i = 0; i < index; i++) {
-            currentNode = currentNode.nextNode;
-        }
+      while (currentNode.nextNode !== tail) {
+        currentNode = currentNode.nextNode;
+      }
 
-        return currentNode;
+      currentNode.nextNode = null;
+      tail = currentNode;
     }
 
-    function pop() {
-        if (!head) {
-            return;
-        }
+    size--;
+  }
 
-        if (size === 1) {
-            head = null;
-            tail = null;
-        } else {
-            let currentNode = head;
+  function contains(value) {
+    let currentNode = head;
 
-            while (currentNode.nextNode !== tail) {
-                currentNode = currentNode.nextNode;
-            }
-
-            currentNode.nextNode = null;
-            tail = currentNode;
-        }
-
-        size--;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return true;
+      }
+      currentNode = currentNode.nextNode;
     }
 
-    function contains(value) {
-        let currentNode = head;
+    return false;
+  }
 
-        while (currentNode) {
-            if (currentNode.value === value) {
-                return true;
-            }
-            currentNode = currentNode.nextNode;
-        }
+  function find(value) {
+    let currentNode = head;
+    let index = 0;
 
-        return false;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return index;
+      }
+      currentNode = currentNode.nextNode;
+      index++;
     }
 
-    function find(value) {
-        let currentNode = head;
-        let index = 0;
+    return null;
+  }
 
-        while (currentNode) {
-            if (currentNode.value === value) {
-                return index;
-            }
-            currentNode = currentNode.nextNode;
-            index++;
-        }
+  function toString() {
+    let result = "";
+    let currentNode = head;
 
-        return null;
-
+    while (currentNode) {
+      result += `( ${currentNode.value} ) -> `;
+      currentNode = currentNode.nextNode;
     }
 
-    function toString() {
-        let result = '';
-        let currentNode = head;
+    result += "null";
+    return result;
+  }
 
-        while (currentNode) {
-            result += `( ${currentNode.value} ) -> `;
-            currentNode = currentNode.nextNode;
-        }
+  // Zee: DIL!
+  function insertAt(value, index) {}
 
-        result += 'null';
-        return result;
+  // Zee: DIL!
+  function removeAt(index) {}
 
-    }
-
-    // Zee: Do these later
-    function insertAt(value, index) {
-
-    }
-
-    function removeAt(index) {
-
-    }
-
-    return {
-        append,
-        prepend,
-        getSize,
-        getHead,
-        getTail,
-        at,
-        pop,
-        contains,
-        find,
-        toString,
-        insertAt,
-        removeAt,
-    };
-
+  return {
+    append,
+    prepend,
+    getSize,
+    getHead,
+    getTail,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
+    insertAt,
+    removeAt,
+  };
 }
