fc696adf4718e85346bf836a561384aa0e72b707
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-18 02:44:51 +0530

    feat: Define LL factory skeleton with placeholders

--- src/linkedList.js
+++ src/linkedList.js
@@ -1,3 +1,50 @@
 function linkedList() {
+    function append(value) {
+
+    }
+
+    function prepend(value) {
+
+    }
+
+    function size() {
+
+    }
+
+    function head() {
+
+    }
+
+    function tail() {
+
+    }
+
+    function at(index) {
+
+    }
+
+    function pop() {
+        
+    }
+
+    function contains(value) {
+
+    }
+
+    function find(value) {
+
+    }
+
+    function toString() {
+
+    }
+
+    function insertAt(value, index) {
+
+    }
+
+    function removeAt(index) {
+        
+    }
 
 }
