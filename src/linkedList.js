f4908c750324c5f3de74c54183f23c77ae62f5f9
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-18 03:41:04 +0530

    refactor: remove else block

--- src/linkedList.js
+++ src/linkedList.js
@@ -54,13 +54,12 @@ export default function linkedList() {
     function at(index) {
         if (index < 0 || index >= size) {
             return null;
-        } else {
-            let currentNode = head;
-            for (let i = 0; i < index; i++) {
-                currentNode = currentNode.nextNode;
-            }
-            return currentNode;
         }
+        let currentNode = head;
+        for (let i = 0; i < index; i++) {
+            currentNode = currentNode.nextNode;
+        }
+        return currentNode;
     }
 
     function pop() {
